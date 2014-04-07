4649a72af113e3634bb6664f2b71e082343ee00b
Jens Grivolla <jens.grivolla@upf.edu>
2014-04-07 18:08:13 +0200

    dbpedia pipeline: initial version

---
+++ examples/dbpedia-pipeline/README.md
@@ -0,0 +1,8 @@
+DBpedia pipeline example
+========================
+
+A simple example pipeline using dbpedia-spotlight.
+
+The example is configured to use the public spotlight demo, but it is of course preferable to use a local instance of the service.
+
+The dependency of the spotlight annotator can currently be satisfied by cloning https://github.com/jgrivolla/dbpedia-spotlight and running `mvn clean install` from within the `uima/` directory.
---
+++ examples/dbpedia-pipeline/src/main/java/eu/eumssi/uima/pipelines/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * A simple example pipeline using dbpedia-spotlight.
+ * 
+ * The example is configured to use the public spotlight demo, 
+ * but it is of course preferable to use a local instance of the service.
+ * The dependency of the spotlight annotator can currently be satisfied 
+ * by cloning https://github.com/jgrivolla/dbpedia-spotlight and running 
+ * `mvn clean install` from within the `uima/` directory.
+ */
+/**
+ * @author jgrivolla
+ *
+ */
+package eu.eumssi.uima.pipelines;
