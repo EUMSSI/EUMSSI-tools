a4143ae1673073fdfee482294b53fc8d12a41dff
Jens Grivolla <jens.grivolla@upf.edu>
2015-09-07 13:42:27 +0200

    fix move-tweets-to-new-collection.js

--- scripts/move-tweets-to-new-collection.js
+++ scripts/move-tweets-to-new-collection.js
@@ -1,7 +1,7 @@
 db.content_items.aggregate([{$match: {source: "Twitter"}}, {$out: "twitter_temp"}])
 db.twitter_temp.copyTo("tweets")
 db.twitter_temp.drop()
-db.content_items.remove({source: "Twitter"})db.content_items.remove({source: "Twitter"})
+db.content_items.remove({source: "Twitter"})
 
 db.content_items.aggregate([{$match: {source: "Twitter-DW"}}, {$out: "twitter_temp"}])
 db.twitter_temp.copyTo("tweets")
